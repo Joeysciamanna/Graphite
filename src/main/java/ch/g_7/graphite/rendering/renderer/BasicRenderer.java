b5765fd6f0bcc8473a85c10d43f3d8ece7ab2ac9
joeysciamanna <joeysciamanna@icloud.com>
2019-10-07 18:41:09 +0200

    .

--- src/main/java/ch/g_7/graphite/rendering/renderer/BasicRenderer.java
+++ src/main/java/ch/g_7/graphite/rendering/renderer/BasicRenderer.java
@@ -25,13 +25,12 @@ import ch.g_7.graphite.rendering.Dimension;
 import ch.g_7.graphite.rendering.shaderprogram.BasicShaderProgram;
 
 public class BasicRenderer implements IRenderer<IGameObject> {
-    
+
 	private BasicShaderProgram shaderProgram;
- 
+
 	private Matrix4f viewMatrix;
-	
+
 	private Matrix4f modelViewMatrix;
-	
 
 	public BasicRenderer() {
 		shaderProgram = new BasicShaderProgram();
@@ -48,44 +47,41 @@ public class BasicRenderer implements IRenderer<IGameObject> {
 	public void render(List<IGameObject> renderables, Dimension dimension, Window window, Camera camera) {
 
 		shaderProgram.bind();
-	    
-	    viewMatrix.identity();
-	    viewMatrix.translate((float)-camera.getPosition().x,(float) -camera.getPosition().y,(float)-camera.getPosition().z);
-	        
+
+		viewMatrix.identity();
+		viewMatrix.translate((float) -camera.getPosition().x, (float) -camera.getPosition().y,
+				(float) -camera.getPosition().z);
+
 		shaderProgram.setTextureSampler(0);
-	    
+
 		// Render each gameItem
 		for (IGameObject object : renderables) {
-		
-				
-				// Set model view matrix for this item
-		        Matrix4f viewCurr = new Matrix4f(viewMatrix);
-		        modelViewMatrix = viewCurr.mul(object.getModelViewMatrix());
-				
-			
-				shaderProgram.setModelViewMatrix(modelViewMatrix);
 
-				shaderProgram.setColor(object.getViewModel().getColor());
-						
-				if(object.getViewModel().getTexture() != null) {
-					// Render the mes for this game item
-					// Activate firs texture bank
-					glActiveTexture(GL_TEXTURE0);
-					// Bind the texture
-					glBindTexture(GL_TEXTURE_2D, object.getViewModel().getTexture().getId());
-				}
-					// Draw the mesh
-					glBindVertexArray(object.getViewModel().getMesh().getVaoId());
-					glEnableVertexAttribArray(0);
-					glEnableVertexAttribArray(1);
-	
-					glDrawElements(GL_TRIANGLES, object.getViewModel().getMesh().getVertexCount(), GL_UNSIGNED_INT, 0);
-	
-					glDisableVertexAttribArray(0);
-					glDisableVertexAttribArray(1);
-					glBindVertexArray(0);
-					
-			
+			// Set model view matrix for this item
+			Matrix4f viewCurr = new Matrix4f(viewMatrix);
+			modelViewMatrix = viewCurr.mul(object.getModelViewMatrix());
+
+			shaderProgram.setModelViewMatrix(modelViewMatrix);
+
+			shaderProgram.setColor(object.getViewModel().getColor());
+
+			if (object.getViewModel().getTexture() != null) {
+				// Render the mes for this game item
+				// Activate firs texture bank
+				glActiveTexture(GL_TEXTURE0);
+				// Bind the texture
+				glBindTexture(GL_TEXTURE_2D, object.getViewModel().getTexture().getId());
+			}
+			// Draw the mesh
+			glBindVertexArray(object.getViewModel().getMesh().getVaoId());
+			glEnableVertexAttribArray(0);
+			glEnableVertexAttribArray(1);
+
+			glDrawElements(GL_TRIANGLES, object.getViewModel().getMesh().getVertexCount(), GL_UNSIGNED_INT, 0);
+
+			glDisableVertexAttribArray(0);
+			glDisableVertexAttribArray(1);
+			glBindVertexArray(0);
 
 		}
 
@@ -96,7 +92,5 @@ public class BasicRenderer implements IRenderer<IGameObject> {
 	public void close() {
 		shaderProgram.close();
 	}
-
-
 
 }
