00308c9dac60a5042a42519bc8ab08a9a57eecb3
joeysciamanna <joeysciamanna@icloud.com>
2020-01-05 21:51:10 +0100

    3D rendering -> we got something :)

--- src/main/java/ch/g_7/graphite/rendering/renderer/Entity3dRenderer.java
+++ src/main/java/ch/g_7/graphite/rendering/renderer/Entity3dRenderer.java
@@ -1,69 +1,51 @@
-//package ch.g_7.graphite.rendering.renderer;
-//
-//import java.util.List;
-//
-//import org.joml.Matrix4f;
-//import org.joml.Vector3f;
-//
-//import ch.g_7.graphite.core.Camera;
-//import ch.g_7.graphite.core.window.Window;
-//import ch.g_7.graphite.entity.IEntity;
-//import ch.g_7.graphite.rendering.ITransformation;
-//import ch.g_7.graphite.rendering.shaderprogram.EntityShaderProgram;
-//
-//public class Entity3dRenderer extends BasicRenderer<EntityShaderProgram, IEntity> implements ITransformation<IEntity> {
-//
-//	private final Matrix4f projectionMatrix;
-//
-//	private final Matrix4f modelViewMatrix;
-//
-//	private float fov = (float) Math.toRadians(90.0f);
-//
-//	private float zNear = 0.01f;
-//
-//	private float zFar = 1000.f;
-//
-//	public Entity3dRenderer() {
-//		super(new EntityShaderProgram());
-//		projectionMatrix = new Matrix4f();
-//		modelViewMatrix = new Matrix4f();
-//	}
-//
-//	@Override
-//	protected void renderAll(List<IEntity> renderables) {
-//		for (IEntity entity : renderables) {
-//			render(entity, this);
-//		}
-//	}
-//
-//	@Override
-//	protected void prepareTransformations(Window window, Camera camera) {
-//		prepareTransformation(window, camera);
-//	}
-//
-//	@Override
-//	public Matrix4f getModelViewMatrix(IEntity entity) {
-//		System.out.println("   do");
-//		return modelViewMatrix.identity().translate(entity.getPosition()).rotateXYZ(new Vector3f(entity.getRotation()))
-//				.scale(entity.getScale()).mul(projectionMatrix);
-//	}
-//
-//	@Override
-//	public void prepareTransformation(Window window, Camera camera) {
-//		projectionMatrix.identity().perspective(fov, window.getWidth() / window.getHeight(), zNear, zFar);
-//		System.out.println("prep");
-//	}
-//
-//	public void setFov(float fov) {
-//		this.fov = fov;
-//	}
-//
-//	public void setzFar(float zFar) {
-//		this.zFar = zFar;
-//	}
-//
-//	public void setzNear(float zNear) {
-//		this.zNear = zNear;
-//	}
-//
-//}
+package ch.g_7.graphite.rendering.renderer;
+
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
+import ch.g_7.graphite.core.Camera;
+import ch.g_7.graphite.core.window.Window;
+import ch.g_7.graphite.entity.IEntity;
+import ch.g_7.graphite.rendering.ITransformation;
+import ch.g_7.graphite.rendering.shaderprogram.EntityShaderProgram;
+
+public class Entity3dRenderer extends BasicRenderer<EntityShaderProgram, IEntity, IEntity> {
+
+	public Entity3dRenderer() {
+		super(new EntityShaderProgram(), new Transformation());
+	}
+	
+	private static class Transformation implements ITransformation<IEntity>{
+		
+		private float fov = (float) Math.toRadians(90.0f);
+
+		private float zNear = 0.01f;
+
+		private float zFar = 1000.f;
+		
+		private Matrix4f projectionMatrix;
+		private Matrix4f modelViewMatrix;
+		private Matrix4f viewMatrix;
+		
+		public Transformation() {
+			projectionMatrix = new Matrix4f();
+			modelViewMatrix = new Matrix4f();
+			viewMatrix = new Matrix4f();
+		}
+		
+		@Override
+		public Matrix4f getProjectionMatrix(Window window, Camera camera) {
+			viewMatrix.identity().translate((float)-camera.getPosition().x(), (float)-camera.getPosition().y(), (float)-camera.getPosition().z());
+			return projectionMatrix.identity().perspective(fov, window.getWidth() / window.getHeight(), zNear, zFar);
+		}
+		
+		@Override
+		public Matrix4f getModelViewMatrix(IEntity entity) {
+			return modelViewMatrix.identity().identity().translate(entity.getPosition()).rotateXYZ(new Vector3f(entity.getRotation()))
+					.scale(entity.getScale()).mul(viewMatrix);
+		}
+
+	}
+
+
+}
