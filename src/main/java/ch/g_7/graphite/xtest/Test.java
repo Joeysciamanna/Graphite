adb8e5b20d23bab19329910cc0638ee0540c2660
joeysciamanna <joeysciamanna@icloud.com>
2020-01-08 21:53:09 +0100

    ctrl + shift + o

--- src/main/java/ch/g_7/graphite/xtest/Test.java
+++ src/main/java/ch/g_7/graphite/xtest/Test.java
@@ -22,27 +22,24 @@ public class Test extends Application {
 	public Test(String name) {
 		super(name);
 	}
-	
-	
+
 	public static void main(String[] args) {
 		new Test("Test App").setRunning(true);
 	}
-	
 
 	BasicEntity entity1;
 	BasicEntity entity2;
-	
+
 	@Override
 	protected void initGame() {
 
-		
-		
-		Texture square1 = new SecureRunner<Void, Texture>(()->new Texture("C:\\Users\\Joey Sciamanna\\git\\Graphite\\src\\main\\resources\\textures\\square.png")).get();
+		Texture square1 = new SecureRunner<Void, Texture>(() -> new Texture(
+				"C:\\Users\\Joey Sciamanna\\git\\Graphite\\src\\main\\resources\\textures\\square.png")).get();
 //		Texture square2 = new SecureRunner<Void, Texture>(()->new Texture("C:\\Users\\Joey Sciamanna\\git\\Graphite\\src\\main\\resources\\textures\\square2.png")).get();
-		
+
 		UIRootContainer inventory = new UIRootContainer(getWindow());
 		getDimension().addObj(inventory, RenderCluster.UI);
-		
+
 		UIPanel panel2 = new UIPanel();
 		panel2.getPreferedWidth().reset().addPF(50);
 		panel2.getPreferedHeight().reset().addPF(50);
@@ -50,13 +47,13 @@ public class Test extends Application {
 		panel2.getY().addPF(12.5f);
 		panel2.setColor(Color.getColor(0, 0, 0, 0));
 		inventory.add(panel2);
-		
+
 		DontCareLayoutPanel layoutPanel = new DontCareLayoutPanel();
 		layoutPanel.getPreferedWidth().reset().addPF(100);
 		layoutPanel.getPreferedHeight().reset().addPF(50);
 		layoutPanel.setColor(Color.getColor(255, 0, 0));
 		inventory.add(layoutPanel);
-		
+
 		UIButton panel1 = new UIButton();
 		panel1.getPreferedWidth().reset().addPF(25);
 		panel1.getPreferedHeight().reset().addPF(50);
@@ -65,56 +62,54 @@ public class Test extends Application {
 		panel1.getX().addPF(12.5f);
 		panel1.getY().addPF(12.5f);
 		layoutPanel.add(panel1);
-		
+
 		panel1.getMaxHeight().addPF(200);
 		panel1.getPreferedHeight().addPF(100);
-		
+
 		inventory.setVisible(false);
 
 		float[] positions = new float[] {
-			    // VO
-			    -0.5f,  0.5f,  0.5f,
-			    // V1
-			    -0.5f, -0.5f,  0.5f,
-			    // V2
-			    0.5f, -0.5f,  0.5f,
-			    // V3
-			     0.5f,  0.5f,  0.5f,
-			    // V4
-			    -0.5f,  0.5f, -0.5f,
-			    // V5
-			     0.5f,  0.5f, -0.5f,
-			    // V6
-			    -0.5f, -0.5f, -0.5f,
-			    // V7
-			     0.5f, -0.5f, -0.5f,
-			};
+				// VO
+				-0.5f, 0.5f, 0.5f,
+				// V1
+				-0.5f, -0.5f, 0.5f,
+				// V2
+				0.5f, -0.5f, 0.5f,
+				// V3
+				0.5f, 0.5f, 0.5f,
+				// V4
+				-0.5f, 0.5f, -0.5f,
+				// V5
+				0.5f, 0.5f, -0.5f,
+				// V6
+				-0.5f, -0.5f, -0.5f,
+				// V7
+				0.5f, -0.5f, -0.5f, };
 		int[] indices = new int[] {
-			    // Front face
-			    0, 1, 3, 3, 1, 2,
-			    // Top Face
-			    4, 0, 3, 5, 4, 3,
-			    // Right face
-			    3, 2, 7, 5, 3, 7,
-			    // Left face
-			    6, 1, 0, 6, 0, 4,
-			    // Bottom face
-			    2, 1, 6, 2, 6, 7,
-			    // Back face
-			    7, 6, 4, 7, 4, 5,
-			};
-	    ViewModel viewModel = new ViewModel();
+				// Front face
+				0, 1, 3, 3, 1, 2,
+				// Top Face
+				4, 0, 3, 5, 4, 3,
+				// Right face
+				3, 2, 7, 5, 3, 7,
+				// Left face
+				6, 1, 0, 6, 0, 4,
+				// Bottom face
+				2, 1, 6, 2, 6, 7,
+				// Back face
+				7, 6, 4, 7, 4, 5, };
+		ViewModel viewModel = new ViewModel();
 		viewModel.setMesh(new BasicMesh3d(positions, indices));
 //		viewModel.setTexture(square1);
 		viewModel.setColor(Color.getColor(255, 255, 0));
-		
+
 		entity1 = new BasicEntity();
 		entity1.setViewModel(viewModel);
 		entity1.setPosition(new Vector3f(0, 0, -2));
 		RenderCluster.ENTITIES.getRenderer().setTransformation(new EntityTransformation3d());
 		getDimension().addObj(entity1, RenderCluster.ENTITIES);
 //		
-		
+
 //		IMesh2d mesh2 = MeshFactory2d.getSquare(1).setCenter(MeshyBuilder2d.CENTER_MIDDLE).build();
 //		entity2 = new BasicEntity();
 //		entity2.setTexture(square2);
@@ -122,35 +117,35 @@ public class Test extends Application {
 //		entity2.setPosition(new Vector3f(0, 0, -2f));
 //		getDimension().addObj(entity2, RenderClass.ENTITIES_3D);
 //		
-	
+
 		getWindow().setVisible(true);
 		getWindow().setSize(500, 500);
 	}
-		
 
 	@Override
 	public void update(double deltaMillis) {
 //		entity1.setScale((float) (entity1.getScale() + deltaMillis * 0.0001));
 //		System.out.println(getTimer().getFPS());
-		
-		if(getWindow().isKeyPressed(GLFW.GLFW_KEY_W)) {
-			entity1.getRotation().add((float) Math.toRadians(deltaMillis * -0.05) ,0,0);
+
+		if (getWindow().isKeyPressed(GLFW.GLFW_KEY_W)) {
+			entity1.getRotation().add((float) Math.toRadians(deltaMillis * -0.05), 0, 0);
 		}
-		if(getWindow().isKeyPressed(GLFW.GLFW_KEY_S)) {
-			entity1.getRotation().add((float) Math.toRadians(deltaMillis * 0.05) ,0,0);
+		if (getWindow().isKeyPressed(GLFW.GLFW_KEY_S)) {
+			entity1.getRotation().add((float) Math.toRadians(deltaMillis * 0.05), 0, 0);
 		}
-		if(getWindow().isKeyPressed(GLFW.GLFW_KEY_A)) {
-			entity1.getRotation().add(0, (float) Math.toRadians(deltaMillis * -0.05),0);
+		if (getWindow().isKeyPressed(GLFW.GLFW_KEY_A)) {
+			entity1.getRotation().add(0, (float) Math.toRadians(deltaMillis * -0.05), 0);
 		}
-		if(getWindow().isKeyPressed(GLFW.GLFW_KEY_D)) {
-			entity1.getRotation().add(0, (float) Math.toRadians(deltaMillis * 0.05),0);
+		if (getWindow().isKeyPressed(GLFW.GLFW_KEY_D)) {
+			entity1.getRotation().add(0, (float) Math.toRadians(deltaMillis * 0.05), 0);
 		}
-		if(getWindow().isKeyPressed(GLFW.GLFW_KEY_Q)) {
+		if (getWindow().isKeyPressed(GLFW.GLFW_KEY_Q)) {
 			entity1.getRotation().add(0, 0, (float) Math.toRadians(deltaMillis * -0.05));
 		}
-		if(getWindow().isKeyPressed(GLFW.GLFW_KEY_E)) {
+		if (getWindow().isKeyPressed(GLFW.GLFW_KEY_E)) {
 			entity1.getRotation().add(0, 0, (float) Math.toRadians(deltaMillis * 0.05));
 		}
+
 //		entity1.getRotation().x = (float) Math.toRadians(deltaMillis * 0.05 + Math.toDegrees(entity1.getRotation().x));
 //		entity1.getRotation().y = (float) Math.toRadians(deltaMillis * 0.05 + Math.toDegrees(entity1.getRotation().y));
 //		entity1.getRotation().z = (float) Math.toRadians(deltaMillis * 0.05 + Math.toDegrees(entity1.getRotation().z));
@@ -160,6 +155,5 @@ public class Test extends Application {
 //		System.out.println(getTimer().calculateFPS());
 
 	}
-	
 
 }
