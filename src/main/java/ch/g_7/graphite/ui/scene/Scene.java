a5b98cdd3431ce115a5138f56b24796c0f67e0f2
joeysciamanna <joeysciamanna@icloud.com>
2020-01-19 20:42:35 +0100

    .

--- src/main/java/ch/g_7/graphite/ui/scene/Scene.java
+++ src/main/java/ch/g_7/graphite/ui/scene/Scene.java
@@ -6,11 +6,22 @@ import ch.g_7.graphite.ui.UIRootContainer;
 public class Scene extends UIRootContainer {
 
 	protected static final SceneNavigator navigator = SceneNavigator.getInstance();
+	protected final String name;
 	
-	public Scene(Window window) {
+	public Scene(String name, Window window) {
 		super(window);
+		this.name = name;
+		navigator.addScene(name, this);
 	}
 
 	protected void onOpen() {}
 	protected void onClose() {}
+	
+	protected final void activate() {
+		navigator.goTo(name);
+	}
+	
+	protected final void deaktivate() {
+		navigator.close(this);
+	}
 }
