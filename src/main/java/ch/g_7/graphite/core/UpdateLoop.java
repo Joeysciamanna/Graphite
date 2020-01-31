62272b42fe200620435c2f5401ed2c0b99d09a6f
joeysciamanna <joeysciamanna@icloud.com>
2020-01-31 23:45:32 +0100

    .

--- src/main/java/ch/g_7/graphite/core/UpdateLoop.java
+++ src/main/java/ch/g_7/graphite/core/UpdateLoop.java
@@ -3,8 +3,6 @@ package ch.g_7.graphite.core;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
 import ch.g_7.graphite.node.Updatable;
 import ch.g_7.util.loop.Loop;
 
