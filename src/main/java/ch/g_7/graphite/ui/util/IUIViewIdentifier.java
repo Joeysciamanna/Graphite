b198aee5e082a3b325976d0bcccefaa52f388f26
joeysciamanna <joeysciamanna@icloud.com>
2020-04-10 16:01:20 +0200

    rm unused ui

--- src/main/java/ch/g_7/graphite/ui/util/IUIViewIdentifier.java
+++
@@ -1,6 +0,0 @@
-package ch.g_7.graphite.ui.util;
-
-import ch.g_7.graphite.node.IEntityIdentifier;
-
-public interface IUIViewIdentifier<T extends Enum<T> & IUIViewIdentifier<T>> extends IEntityIdentifier<T> {
-}
---
+++ src/main/java/ch/g_7/graphite/xjfx/util/Test.java
@@ -0,0 +1,16 @@
+package ch.g_7.graphite.xjfx.util;
+
+import javafx.application.Application;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Region region = new Region(){
+
+
+
+        };
+    }
+}
